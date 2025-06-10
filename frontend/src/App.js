7b3513005d29c05235d65992ecc8b618f138b67a
Paras Tyagi <129530841+paras1105tyagi@users.noreply.github.com>
2025-06-10 16:15:04 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,7 @@ import BarChartIcon from '@mui/icons-material/BarChart';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://ques-company-website.onrender.com/api';
 
 function App() {
   const [companies, setCompanies] = useState([]);
